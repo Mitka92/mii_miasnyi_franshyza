50ae74dce0f07fd18f732d68ed93467ad45f7966
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2025-08-31 17:01:54 +0300

    Update gallery-init.js

--- js/gallery-init.js
+++ js/gallery-init.js
@@ -1,15 +1,17 @@
 // Код для gallery-init.js
-document.addEventListener('DOMContentLoaded', function() {
-    new SimpleLightbox('#gallery-container a', {
-      nav: true,          // Увімкнути навігаційні кнопки
-      close: true,        // Увімкнути кнопку закриття
-      spinner: false,     // Вимкнути спіннер
-      captions: true,     // Показати підписи
-      animationSlide: false, // Вимкнути анімацію переходу між слайдами
-      animationSpeed: 300, // Швидкість анімації закриття/відкриття
-      closeOnSwipe: false, // Вимкнути закриття свайпом
-      showCounter: false,  // Показати лічильник зображень (1/14)
-      scrollZoomFactor:false,
-      loop:false,
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  new SimpleLightbox('#gallery-container a', {
+    nav: true, // Увімкнути навігаційні кнопки
+    close: true, // Увімкнути кнопку закриття
+    spinner: false, // Вимкнути спіннер
+    captions: true, // Показати підписи
+    animationSlide: false, // Вимкнути анімацію переходу між слайдами
+    animationSpeed: 300, // Швидкість анімації закриття/відкриття
+    closeOnSwipe: false, // Вимкнути закриття свайпом
+    showCounter: false, // Показати лічильник зображень (1/14)
+    scrollZoomFactor: false,
+    loop: false,
+    overlay: true,
+    overlayOpacity: 1,
   });
+});
